25802b4679d62fb86c4124ab754cfaa0a3371b8a
CarlosHdz7 <carloshdz.udb@gmail.com>
2019-02-24 14:19:05 -0600

    se agrego el pipe para json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,16 @@ export class AppComponent {
   texto = 'One for All';
   arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   PI = Math.PI;
+  porcentaje = 0.234;
+  moneda = 1234.5;
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: '19'
+    }
+  };
 }
