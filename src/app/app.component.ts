85a55d782d48d19fc9245d5572f99f24d12bc884
CarlosHdz7 <carloshdz.udb@gmail.com>
2019-02-24 13:35:41 -0600

    Se agregaron los pipes para texto y para decimales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'pipes-app';
+  texto = 'One for All';
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  PI = Math.PI;
 }
